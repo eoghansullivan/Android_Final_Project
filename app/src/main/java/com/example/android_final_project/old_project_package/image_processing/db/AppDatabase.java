05104570d6ce7daa35869711feba7ff1f304a581
eoghan <sullivaneoghan@yahoo.co.uk>
2023-12-07 06:31:18 +0000

    Deleted old files cleanup

--- app/src/main/java/com/example/android_final_project/old_project_package/image_processing/db/AppDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.example.android_final_project.old_project_package.image_processing.db;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {ImageEntity.class}, version = 1, exportSchema = false)
-public abstract class AppDatabase extends RoomDatabase {
-    public abstract ImageDao imageDao();
-}
